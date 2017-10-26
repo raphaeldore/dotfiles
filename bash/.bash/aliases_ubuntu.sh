deff05fd77dfbbd947add8432d091f97930aae98
Raphaël Doré <rdore@neomalibox.ch>
2017-10-26 10:29:06 -0400

    Added file aliases_ubuntu.sh

---
+++ bash/.bash/aliases_ubuntu.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+alias apti='sudo apt install'       # Installs a package
+alias aptr='sudo apt remove'        # Removes a package
+alias aptp='sudo apt purge'         # Removes package with configuration
+alias aptu='sudo apt update'        # Refreshes repository index
+alias aptfg='sudo apt full-upgrade' # Upgrades packages with auto-handling of dependencies
+alias aptg='sudo apt upgrade'       # Upgrades all upgradable packages
+alias apts='sudo apt search'        # Searches for the program
