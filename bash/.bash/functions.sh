3496b8fa0929853cb9d4b2a6aadd14d3641edc32
Raphaël Doré <rdore@neomailbox.ch>
2018-10-15 13:21:11 -0400

    Added a few bash fonctions that allow to bookmark folders and easily go back to them. (taken from a reddit post)

--- bash/.bash/functions.sh
+++ bash/.bash/functions.sh
@@ -93,3 +93,36 @@ sha256-check() {
         echo "$2  $1" | sha256sum -c -
     fi
 }
+
+##############################################################################
+# mark/jump support + completion
+# un/mark name : bookmark a directory or remove one (unmark)
+# jump name : jump to directory
+# marks : show all bookmarks
+# Source: https://www.reddit.com/r/commandline/comments/9md3pp/a_very_useful_bashrc_file/e7dy21q/
+export MARKPATH=$HOME/.marks
+function jump {
+    cd -P $MARKPATH/$1 2>/dev/null || echo "No such mark: $1"
+}
+
+function mark {
+    mkdir -p $MARKPATH; ln -s $(pwd) $MARKPATH/$1
+}
+
+function unmark {
+    rm -i $MARKPATH/$1
+}
+
+function marks {
+    ls -l $MARKPATH | sed 's/  / /g' | cut -d' ' -f9- | sed 's/ -/\t-/g' && echo
+}
+
+_completemarks() {
+  local curw=${COMP_WORDS[COMP_CWORD]}
+  local wordlist=$(find $MARKPATH -type l -printf "%f\n")
+  COMPREPLY=($(compgen -W '${wordlist[@]}' -- "$curw"))
+  return 0
+}
+
+complete -F _completemarks jump unmark
+##############################################################################
